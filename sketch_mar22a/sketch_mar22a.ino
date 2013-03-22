6fec43dd3c29d4063105bc1ad3c29a1295e98bc4
Philip Taylor <philip.taylor@taylorworld.me.uk>
2013-03-22 17:33:20 +0000

    adjustments

--- sketch_mar22a/sketch_mar22a.ino
+++ sketch_mar22a/sketch_mar22a.ino
@@ -3,10 +3,12 @@ int y=0;
 int z=0;
 int q=0;
 int f=0;
+int led = 31;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(115200);
+  Serial.begin(115200);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
@@ -16,9 +18,12 @@ void loop() {
   z = random(101, 200);
   x = q + y;
   f = z - x;
-  
+
   Serial.println(x);
   Serial.println(f);
-  delay(2000);
-  
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(500);
 }
+
